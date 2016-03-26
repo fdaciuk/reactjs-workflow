35b89115cae21e22723b1d6f40428aac7335091d
Fernando Daciuk <f.daciuk@gmail.com>
2016-03-25 23:10:25 -0300

    Add es5 example code

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-'use strict';
+'use strict'
+
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+ReactDOM.render(
+  <h1>It worked!</h1>,
+  document.querySelector('[data-js="main"]')
+)
